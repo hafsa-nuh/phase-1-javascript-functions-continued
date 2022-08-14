47d159d9c14a003704e512337a6d7e94044f5854
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-08-14 22:44:48 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun( roller = "roller-skate"){
+    return `This Saturday, I want to ${roller}!`
+}
+function mondayWork( office = 'go to the office'){
+    return `This Monday, I will ${office}.`
+}
+let wrapAdjective = function (initial = "*"){
+    return function (work = "special"){
+        return `You are ${initial}${work}${initial}!`
+    }
+}
+// I DIDNT UNDERSTAND NEED MORE EXPLANATION ON IT 
